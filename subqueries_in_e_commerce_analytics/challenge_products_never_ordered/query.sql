c5b6a18a1aeb944a12f4dac63ebaa00917bf728d
yemitad <>
2026-05-10 22:33:37 +0000

    Challenge: Products Never Ordered

--- subqueries_in_e_commerce_analytics/challenge_products_never_ordered/query.sql
+++ subqueries_in_e_commerce_analytics/challenge_products_never_ordered/query.sql
@@ -1,7 +1,5 @@
-SELECT *
+SELECT p.*
 FROM products p
-JOIN (
-    select product_id
-    from orders o
-    WHERE product_id IS NULL
-) as o on p.product_id = o.product_id 
+LEFT JOIN orders o on p.product_id = o.product_id
+WHERE o.product_id IS Null; 
+
